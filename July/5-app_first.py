4ff061bf68f65d26ce925ace83c39cb857ab1e43
K Sheetal Kothari <21q91a66e1@mrce.in>
2025-07-09 00:39:59 +0530

    Add files via upload

---
+++ July/5-app_first.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.write('''# Hello, from Sheetal!
+###### Welcome to my worlddd!
+ Hahahaha!!
+ * this is my first frontend project
+ * it is amazing
+ * I am feeling great about how my project is going on
+ * this class is conducted my Mr. Senapati Sir''')
