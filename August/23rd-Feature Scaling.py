78ef1f15af69b1375cd4c78790fd5a1210db829a
K Sheetal Kothari <21q91a66e1@mrce.in>
2025-09-03 09:17:50 +0530

    Add files via upload

---
+++ August/23rd-Feature Scaling.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+dataset = pd.read_csv(r"D:\Naresh It Classes\August\13th Spyder, Scikit-learn\Data.csv")
+x = dataset.iloc[:,:-1].values  #divide into x-independent and y-dependent #picking up all the toys in the box except for the last one
+y = dataset.iloc[:,3].values     #just that one special toy you left behind
+         
+#Has nan values(null)
+
+#SimpleImputer is like a toy repair kit that helps fill in those missing pieces
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer()   #uses mean strategy to fill the numerical values
+# imputer = SimpleImputer(strategy="median")
+# imputer = SimpleImputer(strategy="most_frequent")
+
+imputer = imputer.fit(x[:,1:3])       # col 1 & 2 contains missing values
+x[:,1:3] = imputer.transform(x[:,1:3])
+
+
+from sklearn.preprocessing import LabelEncoder     #for converting catogorical to numerical
+labelencoder_x = LabelEncoder()
+
+labelencoder_x.fit_transform(x[:,0])   #works without this as well
+x[:,0] = labelencoder_x.fit_transform(x[:,0])
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+
+from sklearn.model_selection import train_test_split      #for training and testing the model
+# x_train,x_test,y_train,y_test = train_test_split(x,y,train_size=0.8,test_size=0.2)
+# x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.5)
+x_train,x_test,y_train,y_test = train_test_split(x,y,train_size=0.7, random_state=0)
+
+#FEATURE SCALING
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+# Normalization
+from sklearn.preprocessing import Normalizer
+sc_x = Normalizer()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+
+
+
