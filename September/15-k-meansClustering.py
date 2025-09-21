eea7053c59220d30e0423eca80f4ec92f0317e8f
K Sheetal Kothari <21q91a66e1@mrce.in>
2025-09-21 14:31:00 +0530

    Add files via upload

---
+++ September/15-k-meansClustering.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv(r"D:\Naresh It Classes\4. September\15- Grid Search CV, Clustering\Mall_Customers.csv")
+x = dataset.iloc[:,[3,4]].values
+
+#k-means Clustering
+
+from sklearn.cluster import KMeans
+wcss=[]
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i,init='k-means++',random_state=0)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1,11),wcss)
+plt.title('ELbow menthod')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# k=5
+
+kmeans=KMeans(n_clusters=5,init='k-means++',random_state=0)
+y_kmeans=kmeans.fit_predict(x)
+
+# Visualising the clusters
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(x[y_kmeans == 3, 0], x[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(x[y_kmeans == 4, 0], x[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
+
+y_kmeans
+
+dataset['cluster']=y_kmeans
+
+dataset.head()
